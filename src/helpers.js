6b7afdce28a22b40dbeaf0e59a9fc633f768782e
Allan Gallo <allan.gallo.guerra@gmail.com>
2021-05-28 07:07:13 -0600

    ADD - Components Board, Game and Square

---
+++ src/helpers.js
@@ -0,0 +1,28 @@
+export function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		console.log('a', a)
+		console.log('b', b)
+		console.log('c', c)
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+}
+
+const squares = [
+    null, null, null,
+    'X', 'X', '0',
+    null, null, null
+]
